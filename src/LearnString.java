cca3c7c0c2ce4d767d8f6cb900f382091ed920a6
Muthuramakrishnan M <muthu.rama.krishnan96@gmail.com>
2022-09-29 08:34:03 +0530

    String - strip

--- src/LearnString.java
+++ src/LearnString.java
@@ -7,5 +7,31 @@ public class LearnString {
 
         System.out.println(fruit == anotherFruit); //true
         System.out.println(vegetable == anotherVegetable); //false
+
+        String myText = " ";
+        System.out.println(myText.isEmpty());
+        System.out.println(myText.isBlank());
+
+        String firstName = "  Muthu  ";
+        System.out.format("'%s'", firstName);
+        System.out.println();
+        System.out.format("'%s'", firstName.trim());
+        System.out.println();
+        System.out.format("'%s'", firstName.strip());
+        System.out.println();
+        System.out.format("'%s'", firstName.stripLeading());
+        System.out.println();
+        System.out.format("'%s'", firstName.stripTrailing());
+
+        //Java now supports multi line text
+        //from java 15
+
+        String myText2 = """
+                                
+                This is a multiline text block
+                Line 2
+                Line 3
+                """;
+        System.out.println(myText2);
     }
 }
